1c1d2cb60a352abd59af085d1ecaf459dd4267bc
haruki7049 <tontonkirikiri@gmail.com>
2025-12-03 18:24:35 +0900

    Update

---
+++ crates/tensor/src/types.rs
@@ -0,0 +1,13 @@
+type SharedData<T> = Rc<RefCell<Vec<T>>>;
+
+#[derive(Clone)]
+pub struct BaseTensor<T> {
+    pub data: SharedData<T>,
+    pub shape: Vec<usize>,
+    pub strides: Vec<usize>,
+    pub offset: usize,
+}
+
+pub struct Tensor<T> {
+    pub base: BaseTensor<T>,
+}
